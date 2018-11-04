095e32f0a9e14ffeed83ba1fdfb80f1f89031498
Aman <amanraoverma@gmail.com>
2018-11-04 16:13:24 -0500

    Change github link and pylint opts.

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ MINIFY_PAGE = False
 SECRET_KEY = b"\x13\xa0@GH\xdc\xa7\x84\x02\xb0!\xca\xb2\xf9\xa3S\x9d\xe3\xb6\x99\xb2B\xc9j\xd5/O\xeba_f\xda"
 SQLALCHEMY_DATABASE_URI = "sqlite:///site.sqlite"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
